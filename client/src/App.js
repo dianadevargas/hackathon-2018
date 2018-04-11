a1bdb3394c38b01154938b740b0aa439bc5c4499
Adelaine Ho <adelaineho@hipages.com.au>
2018-04-11 18:08:13 +1000

    Add in hui header

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+import { Theme, HeaderV2, Footer } from 'hui/src/styled';
+import { normalize } from 'polished';
+// import logo from './logo.svg';
+// import './App.css';
+
+injectGlobal`
+  ${normalize()}
+  
+  * {
+    min-height: 0;
+    min-width: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background: #ECECEC;
+  }
+  
+  html, body, button, input, optgroup, select, textarea {
+    font-family: Source Sans Pro, sans-serif;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <ThemeProvider theme={Theme}>
+          <HeaderV2 isHomepage siteSource={''} onLoginSuccess={false} />
+          <Footer />
+        </ThemeProvider>
     );
   }
 }
