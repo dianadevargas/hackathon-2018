52cfbe65f9c302d942c35dd042468d917abca41e
Ryan Chung <ryanchung@hipagesgroup.com.au>
2018-04-12 00:47:21 +1000

    whatev

--- apis/index.ts
+++ apis/index.ts
@@ -18,6 +18,10 @@ export async function signedUrl(event, context, callback) {
 
   const response = {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Credentials" : true
+    },
     body: JSON.stringify({ url }),
   };
 
